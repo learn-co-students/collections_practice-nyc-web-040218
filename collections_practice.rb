3ef38a5c841e100a2ab8dc5e81a415d99207e39b
briecodes <hello@brie.work>
2018-04-07 18:33:03 -0400

    collection methods created.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.size <=> y.size }
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  start_i = array[index]
+  end_i = array[destination_index]
+  array[index] = end_i
+  array[destination_index] = start_i
+  array
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 1, 2)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map {|name| name[2] = "$"}
+  array
+end
+
+def find_a(array)
+  array.select{|word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect{|element, index|
+    if index != 1
+      element + "s"
+    else
+      element
+    end
+  }.compact
+end
